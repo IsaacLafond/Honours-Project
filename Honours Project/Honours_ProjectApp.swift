061121a74be6d723ec921e257004164c222d686f
IsaacLafond <91162304+IsaacLafond@users.noreply.github.com>
2024-11-13 18:22:44 -0500

    Initial Commit

---
+++ Honours Project/Honours_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Honours_ProjectApp.swift
+//  Honours Project
+//
+//  Created by Isaac Lafond on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Honours_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Honours ProjectTests/Honours_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Honours_ProjectTests.swift
+//  Honours ProjectTests
+//
+//  Created by Isaac Lafond on 2024-11-13.
+//
+
+import Testing
+@testable import Honours_Project
+
+struct Honours_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
